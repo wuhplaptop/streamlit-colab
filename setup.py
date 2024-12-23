f2359407c6b38ca8a8ba4a0ac609ac0a066cbe30
wuhplaptop <wuhplaptop@gmail.com>
2024-12-23 02:54:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="streamlit-colab",
-    version="0.1.3",
+    version="0.1.4",
     description="Run Streamlit apps on Google Colab with ease",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
